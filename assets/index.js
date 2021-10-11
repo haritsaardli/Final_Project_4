dd960cefb4e662a4b833dad42490fd77c537f52d
blaff <haritsaardli@gmail.com>
2021-10-11 19:59:35 +0700

    current location

--- assets/index.js
+++ assets/index.js
@@ -4,39 +4,80 @@ const temp = document.getElementById("temp");
 const windSpeed = document.getElementById("windSpeed");
 
 function findCity() {
-    const country = document.getElementById("input").value
-    const BASE_URL = new URL('https://api.openweathermap.org/data/2.5/weather');
-    const params = {
-        q:`${country}`,
-        units:"metric",
-        lang:"id",
-        appid:"8a59485fc0098295507e55078609bfee",
-    }
-    BASE_URL.search = new URLSearchParams(params)
-    
-    fetch(BASE_URL)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data);
-            
-            if(data.cod == "404"){
-                city.innerHTML = "City Not Found"
-                cloud.innerHTML = "-"
-                temp.innerHTML = "-"
-                windSpeed.innerHTML = "-"
+	const country = document.getElementById("input").value;
+	const BASE_URL = new URL("https://api.openweathermap.org/data/2.5/weather");
+	let params = {
+		q: `${country}`,
+		units: "metric",
+		lang: "id",
+		appid: "8a59485fc0098295507e55078609bfee"
+	};
+	BASE_URL.search = new URLSearchParams(params);
 
-            }else{
-                city.innerHTML = data.name
-                cloud.innerHTML = data.weather[0].description
-                temp.innerHTML = data.main.temp + " &#8451"
-                windSpeed.innerHTML = data.wind.speed + " m/s"
-
-            }
-        })
-        .catch((err) => {
-            console.log(err + "error");
-        });
-    
+	fetch(BASE_URL)
+		.then((response) => {
+			return response.json();
+		})
+		.then((data) => {
+			if (data.cod == "404") {
+				city.innerHTML = "City Not Found";
+				cloud.innerHTML = "-";
+				temp.innerHTML = "-";
+				windSpeed.innerHTML = "-";
+			} else {
+				city.innerHTML = data.name;
+				cloud.innerHTML = data.weather[0].description;
+				temp.innerHTML = data.main.temp + " &#8451";
+				windSpeed.innerHTML = data.wind.speed + " m/s";
+			}
+		})
+		.catch((err) => {
+			console.log(err + "error");
+		});
 }
+
+function getLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(showPosition);
+	} else {
+		console.log("Geolocation is not supported by this browser.");
+	}
+}
+
+function showPosition(position) {
+	const lati = position.coords.latitude;
+	const long = position.coords.longitude;
+
+	const BASE_URL = new URL("https://api.openweathermap.org/data/2.5/weather");
+	const params = {
+		lat: `${lati}`,
+		lon: `${long}`,
+		units: "metric",
+		lang: "id",
+		appid: "8a59485fc0098295507e55078609bfee"
+	};
+	BASE_URL.search = new URLSearchParams(params);
+
+	fetch(BASE_URL)
+		.then((response) => {
+			return response.json();
+		})
+		.then((data) => {
+			if (data.cod == "404") {
+				city.innerHTML = "City Not Found";
+				cloud.innerHTML = "-";
+				temp.innerHTML = "-";
+				windSpeed.innerHTML = "-";
+			} else {
+				city.innerHTML = data.name;
+				cloud.innerHTML = data.weather[0].description;
+				temp.innerHTML = data.main.temp + " &#8451";
+				windSpeed.innerHTML = data.wind.speed + " m/s";
+			}
+		})
+		.catch((err) => {
+			console.log(err + "error");
+		});
+}
+
+getLocation();
