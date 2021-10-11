6a313a2ebc7c292000d2c1606bbf384d6136e7d6
blaff <haritsaardli@gmail.com>
2021-10-11 17:26:13 +0700

    search funcion

--- assets/index.js
+++ assets/index.js
@@ -1,29 +1,42 @@
-const id = "8a59485fc0098295507e55078609bfee";
-const country = ""
-// const country = "Semarang";
-const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${id}`;
-
 const city = document.getElementById("city");
 const cloud = document.getElementById("cloud");
 const temp = document.getElementById("temp");
 const windSpeed = document.getElementById("windSpeed");
 
-console.log(BASE_URL);
+function findCity() {
+    const country = document.getElementById("input").value
+    const BASE_URL = new URL('https://api.openweathermap.org/data/2.5/weather');
+    const params = {
+        q:`${country}`,
+        units:"metric",
+        lang:"id",
+        appid:"8a59485fc0098295507e55078609bfee",
+    }
+    BASE_URL.search = new URLSearchParams(params)
+    
+    fetch(BASE_URL)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            
+            if(data.cod == "404"){
+                city.innerHTML = "City Not Found"
+                cloud.innerHTML = "-"
+                temp.innerHTML = "-"
+                windSpeed.innerHTML = "-"
 
-fetch(BASE_URL)
-	.then((response) => {
-		return response.json();
-	})
-	.then((data) => {
-		console.log(data);
-        city.innerHTML = data.name
-        cloud.innerHTML = data.weather[0].description
-        temp.innerHTML = Math.round(data.main.temp - 273.15) + " &#8451"
-        windSpeed.innerHTML = data.wind.speed + " m/s"
-	})
-	.catch((err) => {
-		console.log(err + "error");
-	});
+            }else{
+                city.innerHTML = data.name
+                cloud.innerHTML = data.weather[0].description
+                temp.innerHTML = data.main.temp + " &#8451"
+                windSpeed.innerHTML = data.wind.speed + " m/s"
 
-// console.log(fetch);
-// console.log(BASE_URL);
+            }
+        })
+        .catch((err) => {
+            console.log(err + "error");
+        });
+    
+}
