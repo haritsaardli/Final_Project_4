2f9f4386abb3637cf565e4151a23df9e8850038b
haritsaardli <haritsaardli@gmail.com>
2022-01-25 15:02:11 +0700

    fix input

--- assets/index.js
+++ assets/index.js
@@ -3,38 +3,18 @@ const cloud = document.getElementById("cloud");
 const temp = document.getElementById("temp");
 const windSpeed = document.getElementById("windSpeed");
 
-function findCity() {
-	const country = document.getElementById("input").value;
-	const BASE_URL = new URL("https://api.openweathermap.org/data/2.5/weather");
-	let params = {
-		q: `${country}`,
-		units: "metric",
-		lang: "id",
-		appid: "8a59485fc0098295507e55078609bfee"
-	};
-	BASE_URL.search = new URLSearchParams(params);
 
-	fetch(BASE_URL)
-		.then((response) => {
-			return response.json();
-		})
-		.then((data) => {
-			if (data.cod == "404") {
-				city.innerHTML = "City Not Found";
-				cloud.innerHTML = "-";
-				temp.innerHTML = "-";
-				windSpeed.innerHTML = "-";
-			} else {
-				city.innerHTML = data.name;
-				cloud.innerHTML = data.weather[0].description;
-				temp.innerHTML = data.main.temp + " &#8451";
-				windSpeed.innerHTML = data.wind.speed + " m/s";
-			}
-		})
-		.catch((err) => {
-			console.log(err + "error");
-		});
-}
+document.getElementById("input").addEventListener("keyup", function(event) {
+	event.preventDefault();
+    if (event.keyCode === '13') {
+		const country = document.getElementById("input").value;
+        let params = {
+			q: `${country}`,
+		}
+		// console.log(params)
+		weatherAPI(params)
+    }
+})
 
 function getLocation() {
 	if (navigator.geolocation) {
@@ -48,14 +28,24 @@ function showPosition(position) {
 	const lati = position.coords.latitude;
 	const long = position.coords.longitude;
 
-	const BASE_URL = new URL("https://api.openweathermap.org/data/2.5/weather");
-	const params = {
+	let params = {
 		lat: `${lati}`,
 		lon: `${long}`,
+	};
+
+	weatherAPI(params)
+	
+}
+
+function weatherAPI(p) {
+	let params = {
 		units: "metric",
 		lang: "id",
 		appid: "8a59485fc0098295507e55078609bfee"
-	};
+	}
+	Object.assign(params,p)
+
+	const BASE_URL = new URL("https://api.openweathermap.org/data/2.5/weather");
 	BASE_URL.search = new URLSearchParams(params);
 
 	fetch(BASE_URL)
